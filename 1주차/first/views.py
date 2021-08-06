37cac9c78c91bfda11df69751da02a22834a9498
Park Siyeon <xion2664@gmail.com>
2021-08-06 18:14:24 +0900

    session01 assignment - 18_박시연

--- 1주차/first/views.py
+++ 1주차/first/views.py
@@ -3,6 +3,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home (request) :
+
+def home(request):
     firsts = First.objects.all()
-    return render (request, 'home.html', {'firsts':firsts})
+    search_text = request.GET.get('search_text')
+    if search_text:
+        firsts = firsts.filter(title__icontains=search_text)
+        return render(request, 'home.html', {'firsts': firsts})
+
+    return render(request, 'home.html', {'firsts': firsts})
